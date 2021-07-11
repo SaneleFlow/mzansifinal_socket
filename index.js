e56e1ab33b169483833740df3bf7e6ac9865689f
SaneleFlow <technoflow76@gmail.com>
2021-07-11 18:55:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,118 @@
+let app = require('express')();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+
+var port = process.env.PORT || 8080;
+
+
+server.listen(port, function () {
+  console.log('listening in http://localhost:' + port);
+  // response.send('listening in http://localhost:' + port)
+});
+
+sendroom = '';
+users = [];
+
+
+
+
+io.on('connection', (socket) => {
+
+
+
+
+  socket.on('comment-room', (room) => {
+
+    socket.room = 'project:' + room;
+
+    // console.log(socket.room);
+
+
+    socket.join(socket.room)
+  });
+
+
+  socket.on('message-room', (room) => {
+
+    socket.room = 'private:' + room;
+
+    // console.log(socket.room);
+
+
+    socket.join(socket.room)
+  });
+
+
+
+  socket.on('like-channel', () => {
+
+let room = 1;
+
+    socket.room = 'likes:' + room;
+
+    // console.log(socket.room);
+
+
+    socket.join(socket.room)
+
+  });
+
+
+
+
+
+
+  socket.on('disconnect', function () {
+    io.emit('users-changed', { user: socket.username, event: 'left' });
+  });
+
+
+  socket.on('set-name', (name) => {
+    socket.username = name;
+    io.emit('users-changed', { user: name, event: 'joined' });
+  });
+
+  socket.on('set-ID',  (id) => {
+    socket.user_id = id;
+
+  })
+
+  socket.on('set-ID',  (id) => {
+    socket.user_id = id;
+
+  })
+
+
+  socket.on('image', (img) => {
+    socket.img = img;
+ 
+  })
+
+
+
+
+
+
+
+  socket.on('send-message', (message) => {
+// console.log(socket.user_id)
+    io.to(socket.room).emit('message', { comment: message.text, name: socket.username, user_id: socket.user_id, image: socket.img.text, created_at: new Date() });
+  });
+
+
+  socket.on('send-private', (message) => {
+
+    io.to(socket.room).emit('message', { message: message.text, name: socket.username, user_id: socket.user_id,  image: socket.img.text,   created_at: new Date() });
+  });
+
+
+
+
+
+
+
+
+});
+
+
+
